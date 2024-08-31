2d6ff3de68c98a1b7f8107cc48213a37bfa8a2d1
lemarc <lemarc.dev@gmail.com>
2024-08-31 23:56:41 +0200

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/main.js'),
-      name: 'QuazalaServer',
+      entry: resolve(__dirname, "src/main.js"),
+      name: "QuazalaServer",
       fileName: (format) => `main.${format}.js`,
-      formats: ['es', 'cjs'],
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
-      external: [/^node:/, 'events', 'stream', 'path'],
+      external: [/^node:/, "events", "stream", "path"],
     },
-    target: 'node18',
+    target: "node16",
     minify: false,
   },
   test: {
     globals: true,
-    environment: 'node',
+    environment: "node",
   },
 });
